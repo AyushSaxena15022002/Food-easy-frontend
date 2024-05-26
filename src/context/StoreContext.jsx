abf6d4f82b79f877c4613dd97d05d3289e8546e5
Ayush Saxena <ayushsaxena339@gmail.com>
2024-05-26 19:37:37 +0530

    Updation of backend url

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({})
-  const url = 'http://localhost:4000'
+  const url = 'https://food-easy-backend.vercel.app'
   const [token, setToken] = useState('')
   const [food_list, setFoodlist] = useState([])
 
