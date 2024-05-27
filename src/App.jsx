dea2e5441d8ba90363bb01c534eb10c115ba8cd1
Ayush Saxena <ayushsaxena339@gmail.com>
2024-05-27 20:04:36 +0530

    Correcting Browser Router

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@
 // export default App
 
 import React, { useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
@@ -50,16 +50,29 @@ const App = () => {
     <>
       {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
       <div className='app'>
-        <BrowserRouter>
+        <Router>
           <Navbar showLogin={showLogin} setShowLogin={setShowLogin} />
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/cart' element={<Cart />} />
-            <Route path='/order' element={<PlaceOrder />} />
-            <Route path='/verify' element={<Verify />} />
-            <Route path='/myorders' element={<MyOrder />} />
-          </Routes>
-        </BrowserRouter>
+          <Switch>
+            <Route path='/'>
+              <Home />
+            </Route>
+            <Route path='/cart'>
+              <Cart />
+            </Route>
+            <Route path='/order'>
+              <PlaceOrder />
+            </Route>
+            <Route path='/verify'>
+              <Verify />
+            </Route>
+            <Route path='/myorders'>
+              <MyOrder />
+            </Route>
+            <Route path='*'>
+              <Error />
+            </Route>
+          </Switch>
+        </Router>
       </div>
       <Footer />
     </>
